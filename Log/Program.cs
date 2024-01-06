1104f386003ecbeb72db86d654f9e4a4c37799c1
Thomas <thomas@bluenotion.nl>
2024-01-06 18:00:08 +0100

    Remove unused code

--- Log/Program.cs
+++ Log/Program.cs
@@ -1,14 +1,8 @@
-﻿using DataStructures;
-using FileReader;
-using FileReader.Models;
-
-namespace Log;
+﻿namespace Log;
 
 public static class Program
 {
 	public static async Task Main(string[] args)
 	{
-		var jsonFileReader = new JsonFileReader();
-		var data = await jsonFileReader.ReadFromFile<SortingDataModel>("data_sorteren.json");
 	}
 }
